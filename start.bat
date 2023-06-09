d9a82a854786e1596b1bead6bd58c43f949433d9
Mr-Zanzibar <104436099+Mr-Zanzibar@users.noreply.github.com>
2023-06-09 17:22:20 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,25 @@
 @echo off
-TITLE Instagram-Scraper
+TITLE Instagram-Account-Scraper by Mr-Zanzibar
+REM Check if python is installed, if not proceed to the download from the site
+python --version >nul 2>&1
+if %errorlevel% equ 0 (
+    echo Python is already installed.
+) else (
+    echo Python is not installed in the system. Downloading and installing Python.
+
+    bitsadmin /transfer PythonInstaller https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe %temp%\python_installer.exe
+
+    %temp%\python_installer.exe /quiet PrependPath=1 Include_test=0
+
+    python --version >nul 2>&1
+    if %errorlevel% equ 0 (
+        echo Python has been installed successfully.
+    ) else (
+        echo An error occurred during Python installation. Make sure you have an internet connection and try again.
+        pause
+        exit /b
+    )
+)
 
 pip install instaloader
 pip install colorama
@@ -8,7 +28,6 @@ pip install rich
 
 cls
 
-REM Esegui lo script
 python insta.py
 
 pause
